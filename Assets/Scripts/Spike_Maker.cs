7efff65d53ce39235b47e09c5ea81ddc030c9f67
treetreet <treetreet@kakao.com>
2024-10-07 20:58:28 +0900

    샘플용 비트

--- Assets/Scripts/Spike_Maker.cs
+++ Assets/Scripts/Spike_Maker.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Spike_Maker : MonoBehaviour
 {
-    int[] spike = new int[330];
+    int[] spike = new int[]
+    { 0, 1, 0 ,1 ,0 ,1, 0, 1 ,0 ,1 ,0 ,1 ,0, 1 ,1 ,1 ,0 ,1 ,0 ,1 ,0 ,1 ,0 ,1 ,0 ,1 ,0 ,1 ,1 ,1 ,1,
+0, 0, 1 ,0 ,1 ,0 ,1 ,1 ,1 ,0 ,1,0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1 ,1
+,0 ,1 ,0 ,1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1 ,0 ,0 ,1 ,0, 1, 0, 1, 0, 1, 0, 1, 0 ,1, 1, 0,  1, 0 ,1 ,0 ,1 ,1 ,1 ,0, 1,
+0, 1, 1, 1, 0, 1, 0, 1 ,0 ,1 ,1, 1, 1 ,0 ,0, 1, 0, 1 ,1, 1, 1, 0, 0 ,1 ,0 ,0 ,0, 0
+,1 ,1 ,0, 0, 1, 1 ,0, 0, 1, 1, 0, 0, 1,1 ,0 ,0 ,1 ,1, 0, 0, 1, 1 ,0 ,0 ,1, 0, 1, 0, 0, 0, 0
+,0 ,1 ,0 ,1 ,0 ,1 ,1 ,1,0 ,1 ,0 ,1 ,0, 1 ,1, 1, 0, 1, 0, 1 ,0 ,1 ,1 ,1 ,0 ,1, 0, 1, 0, 1, 1, 1
+,1 ,0 ,1, 0, 1, 1, 1,  0, 0, 1 ,0 ,1 ,0 ,1 ,1 ,1, 0 ,1 ,0, 1 ,0 ,1 ,1, 1, 0 ,1 ,0 ,1, 0, 1, 0 ,0
+,0, 1, 0 ,0 ,0, 1 ,0, 0, 0, 1, 0, 0 ,0, 1 ,0, 0, 0, 1,0 ,0 ,0,1 ,0 ,0 ,0 ,1 ,0, 0, 1, 1, 1,1
+,0, 1, 0, 1 ,0 ,1 ,1 ,1, 0 ,1 ,0 ,1 ,0, 1, 1, 1,0 ,1 ,0 ,1 ,0 ,1 ,1, 1 ,0, 1, 1 ,1, 0, 1, 1 ,0,
+0, 1 ,0, 1 ,0 ,1 ,1 ,1 ,0 ,1 ,1, 1,0 ,1, 1,1 ,0, 1, 0, 1 ,0 ,1 ,1 ,1, 0, 1 ,1 ,0, 1, 0, 1, 1, 0,0 ,0, 0 ,1
+ };
+
+
+    //{ 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 };
     int beat = 0;
+    bool flag = false;
 
     [Header("Circle 1")]
     [SerializeField] private GameObject circle1;
@@ -17,40 +32,60 @@ public class Spike_Maker : MonoBehaviour
 
     private void Awake()
     {
-        for (int i = 0; i < spike.Length; i++) { spike[i] = i % 2; }
+        //for (int i = 0; i < spike.Length; i++) { spike[i] = i % 2; }
     }
     public void Beat_Renderer()
     {
         switch (spike[beat])
         {
             case 0:
-                Summon_Spike(2);
+                Debug.Log("non");
                 break;
             case 1:
-                Summon_Spike(1);
+                Debug.Log("yeh");
+                Summon_Spike();
                 break;
 
         }
         beat++;
     }
 
-    void Summon_Spike(int num)
+    void Summon_Spike()
     {
         GameObject spikeClone = ObjectPool.SharedInstance.GetPooledObject();
         spikeClone.SetActive(true);
         var spikeScript = spikeClone.GetComponent<AttachToCircle>();
 
-        if (num == 1)
+        if (flag == false)
         {
             spikeClone.transform.position = new Vector3(spawnPosition1.transform.position.x, spawnPosition1.transform.position.y);
             spikeClone.transform.SetParent(circle1.transform, true);
             spikeScript.attachedObject = circle1;
+            flag = flag != true;
         }
-        else if (num == 2)
+        else if (flag == true)
         {
             spikeClone.transform.position = new Vector3(spawnPosition1.transform.position.x, spawnPosition2.transform.position.y);
             spikeClone.transform.SetParent(circle2.transform, true);
             spikeScript.attachedObject = circle2;
+            flag = flag != true;
         }
+
+        /*
+        if (num == 1 && flag == false || num == 2 && flag == true)
+        {
+            spikeClone.transform.position = new Vector3(spawnPosition1.transform.position.x, spawnPosition1.transform.position.y);
+            spikeClone.transform.SetParent(circle1.transform, true);
+            spikeScript.attachedObject = circle1;
+            flag = flag != true;
+        }
+        else if (num == 2 && flag == false || num == 1 && flag == true)
+        {
+            spikeClone.transform.position = new Vector3(spawnPosition1.transform.position.x, spawnPosition2.transform.position.y);
+            spikeClone.transform.SetParent(circle2.transform, true);
+            spikeScript.attachedObject = circle2;
+            flag = flag != true;
+        }
+        */
     }
 }
